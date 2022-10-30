c813e7ae924a2039e85f16f9dd26596c64e7fadb
MannOfPootis <janez.domini@gmail.com>
2022-10-30 10:49:31 +0100

    I added THe finding of audio files and made it not crash

--- app/src/main/java/com/example/heatbeat/MainActivity.java
+++ app/src/main/java/com/example/heatbeat/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.heatbeat;
 
+import static android.bluetooth.BluetoothGattDescriptor.PERMISSION_READ;
+
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,6 +13,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,16 +22,19 @@ import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.heatbeat.ui.main.SectionsPagerAdapter;
 import com.example.heatbeat.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<ModelAudio> audioArrayList =new ArrayList<ModelAudio>();;
     private ActivityMainBinding binding;
-    ArrayList<ModelAudio> audioArrayList;
+
 
 
     @Override
@@ -42,17 +51,63 @@ public class MainActivity extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = binding.fab;
 
+        if (checkPermission())
+        {
+            setAudio();
+        }
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //bazinga
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "empt lisz", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
+
+                /*for (ModelAudio song:audioArrayList
+                     ) {
+
+                    Snackbar.make(view, "sdssddsdn", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }*/
 
             }
         });
     }
+    public boolean checkPermission() {
+        int READ_EXTERNAL_PERMISSION = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if((READ_EXTERNAL_PERMISSION != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_READ);
+            return false;
+        }
+        return true;
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case  PERMISSION_READ: {
+                if (grantResults.length > 0 && permissions[0].equals(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                        // Toast.makeText(getApplicationContext(), "Please allow storage permission", Toast.LENGTH_LONG).show();
+                    } else
+                    {
+                        setAudio();
+                    }
+                }
+            }
+        }
+    }
+
+    public void setAudio() {
+       // Toast.makeText(this, "was executed", Toast.LENGTH_SHORT).show();
+        getAudioFiles();
+
+
+
+    }
+    //ArrayList<ModelAudio> audioArrayList2 ;
     public void getAudioFiles() {
         ContentResolver contentResolver = getContentResolver();
         Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
@@ -63,23 +118,33 @@ public class MainActivity extends AppCompatActivity {
         if (cursor != null && cursor.moveToFirst()) {
             do {
 
+                //List<String> list
+                //Adding elements in the List
+
+
                 String title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
                 String artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
                 String duration = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION));
                 String url = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+
                 ModelAudio modelAudio = new ModelAudio();
                 modelAudio.setaudioTitle(title);
                 modelAudio.setaudioArtist(artist);
                 modelAudio.setaudioUri(Uri.parse(url));
                 modelAudio.setaudioDuration(duration);
-                //modelAudio.setAvgSpeed(dataBasing.getSpeedByName(title));
-                //modelAudio.setIsok(true);
+
+                modelAudio.setAvgSpeed( 12.0/*dataBasing.getSpeedByName(title)*/);
+                Toast.makeText(this, modelAudio.audioTitle, Toast.LENGTH_SHORT).show();
                 audioArrayList.add(modelAudio);
+
+
+
+
 
             } while (cursor.moveToNext());
         }
 
-        /*AudioAdapter adapter = new AudioAdapter(this, audioArrayList);
+       /* AudioAdapter adapter = new AudioAdapter(this, audioArrayList);
         recyclerView.setAdapter(adapter);
 
         adapter.setOnItemClickListener(new AudioAdapter.OnItemClickListener() {
--- app/src/main/java/com/example/heatbeat/ui/main/PageViewModel.java
+++ app/src/main/java/com/example/heatbeat/ui/main/PageViewModel.java
@@ -1,10 +1,20 @@
 package com.example.heatbeat.ui.main;
 
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import androidx.arch.core.util.Function;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
+
+import com.example.heatbeat.MainActivity;
+import com.example.heatbeat.ModelAudio;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
 
 public class PageViewModel extends ViewModel {
 
@@ -12,7 +22,13 @@ public class PageViewModel extends ViewModel {
     private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
         @Override
         public String apply(Integer input) {
-            return "Hello world from section: " + input;
+            //ArrayList<ModelAudio> x = MainActivity.audioArrayList;
+
+
+
+            return "Hello world from segfhgdfcgfdxction: "+ input ;
+
+
         }
     });
 
